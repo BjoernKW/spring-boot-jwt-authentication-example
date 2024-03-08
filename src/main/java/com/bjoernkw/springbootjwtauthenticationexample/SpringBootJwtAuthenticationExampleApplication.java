654b565d50c06b8c34d7b33c9077d5c38eefdab2
Björn Wilmsmann <bjoern@bjoernkw.com>
2024-03-08 16:01:10 +0100

    Initial commit

---
+++ src/main/java/com/bjoernkw/springbootjwtauthenticationexample/SpringBootJwtAuthenticationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.bjoernkw.springbootjwtauthenticationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthenticationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAuthenticationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjoernkw/springbootjwtauthenticationexample/SpringBootJwtAuthenticationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjoernkw.springbootjwtauthenticationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthenticationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
